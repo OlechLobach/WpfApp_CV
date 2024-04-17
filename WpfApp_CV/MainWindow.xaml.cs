905461840f64bd610ddeebad7e708f1f7ae0fb26
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-17 04:05:07 +0300

    Add project files.

---
+++ WpfApp_CV/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+
+namespace WpfApp_CV
+{
+    public partial class ResumeWindow : Window
+    {
+        public ResumeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Clipboard.SetText(e.Uri.ToString());
+
+            copyPopup.IsOpen = true;
+
+            Task.Delay(1000).ContinueWith(_ =>
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    copyPopup.IsOpen = false;
+                });
+            });
+        }
+    }
+}
